21f11281e57b0602d61d87bdecd9f8d1c8956a43
rusanov-ao <rusanov-ao@inbox.ru>
2022-05-09 12:55:51 +0300

    Initial commit

---
+++ src/main/java/BankCalculator.java
@@ -0,0 +1,34 @@
+
+
+public class BankCalculator {
+
+
+    public int pow(int a) {
+        int result = a * a;
+        return result;
+    }
+
+    public int abs(int a) {
+        int result = a > 0 ? a : a * -1;
+        return result;
+    }
+
+    public int procent(int a, int b) {
+        int result = a / 100 * b;
+        return result;
+    }
+
+    public int comparison(int a, int b) {
+        int result;
+        if (a > b) {
+            result = 1;
+        } else if (a < b) {
+            result = -1;
+        } else {
+            result = 0;
+        }
+        return result;
+    }
+
+
+}
---
+++ src/test/java/BankCalculatorTest.java
@@ -0,0 +1,88 @@
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class BankCalculatorTest {
+    BankCalculator sut;
+
+    @BeforeAll
+    public static void startedAll() {
+        System.out.println("All test started");
+    }
+
+    @BeforeEach
+    public void init() {
+        System.out.println("Test started");
+        sut = new BankCalculator();
+    }
+
+
+    @AfterEach
+    public void finished() {
+        System.out.println("Test completed");
+    }
+
+    @AfterAll
+    public static void finishedAll() {
+        System.out.println("All tests completed");
+    }
+
+    @Test
+    public void testPow() {
+        int a = 5;
+        int expected = 25;
+        int result = sut.pow(a);
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testAbs() {
+        int a = 10;
+        int expected = 10;
+        int result = sut.abs(a);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testProcent() {
+        int a = 100000;
+        int b = 13;
+        int expected = 13000;
+        int result = sut.procent(a, b);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testComparison() {
+        int a = 10;
+        int b = 15;
+        int expected = -1;
+        int result = sut.comparison(a, b);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @MethodSource("source")
+    public void testProcent(int a, int b, int expected) {
+        int result = sut.procent(a, b);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    private static Stream<Arguments> source() {
+        return Stream.of(
+                Arguments.of(50000, 15, 7500),
+                Arguments.of(285000, 89, 253650),
+                Arguments.of(15000, 23, 3450),
+                Arguments.of(2000, 8, 5)); // специально поставит не верный ответ
+
+
+    }
+}
+
